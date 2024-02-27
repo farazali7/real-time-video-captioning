48df251c3b25d7a6256f755bfe13077637c65702
Paanugoban Kugathasan <paank@Paanugobans-MacBook-Pro.local>
2024-02-27 16:02:12 -0500

    Makes a new tests folder and applies comments to metrics per Faraz's comments

--- src/metrics.py
+++ src/metrics.py
@@ -1,29 +1,37 @@
-#NLTK is standard and comes with python
+from typing import List, Optional, Union
+
+#NLTK is standard and comes with python as standard library
 from nltk.translate.bleu_score import corpus_bleu,sentence_bleu
 from nltk.translate.meteor_score import meteor_score
 from nltk import word_tokenize
 import nltk
 nltk.download('wordnet')
-#evaluate also has all BLEU and METEOR scores if standard nltk doesn't work
+
+#Evaluate also has all BLEU and METEOR scores if standard nltk doesn't work, right now used for Rouge-L
 import evaluate
+
+#aac-metrics is a package that has the CIDEr-D score
 from aac_metrics.functional import cider_d
 from aac_metrics.utils.tokenization import preprocess_mono_sents, preprocess_mult_sents
 import subprocess
 
-'''
-This function calculates the BLEU-4 score for an entire corpus. That means we would have a list of all the captions in references and a list of all the predicted captions in candidates.
-'''
-def calculate_bleu_score_corpus(references, candidates):    
-    #Example: [[ref1a sentence, ref1bsentence], [ref2a sentence, ref2b sentence]]
-    #[['The cat is on the mat', 'there is a cat on the wall'],['The cat is not on the mat', 'there is a dog on the wall']]
 
-    #Example: [cand1 sentence, cand2 sentence]
-    #['The cat is on the mat','The cat is not on the mat']
+def calculate_bleu_score_corpus(references:List[List[str]], candidates:List[str])->float:
+    """Calculate the BLEU-4 score for a corpus of sentences, that is multiple candidates.
+    
+    Args:
+        references: A list containing a list of correct captions for each image i.e [['The cat is on the mat', 'there is a cat on the wall'],['The cat is not on the mat', 'there is a dog on the wall']]
+        candidates: A list containing the predicted captions for each image i.e ['The cat is on the mat','The cat is not on the mat']
 
+    Returns:
+        float: The BLEU-4 score for the entire corpus
+    """    
+    #Asserts to ensure the passed parameters follow the correct format
     assert len(references) == len(candidates), "The lengths of references and candidates must be the same"
     assert isinstance(references,list), "References must be a list as it is looking at multiple captions"
     assert isinstance(candidates,list), "Candidates must be a list as it is looking at multiple captions"
 
+    #Tokenize the sentences
     for i in references:
         if isinstance(i, list):
             for j in i:
@@ -33,116 +41,144 @@ def calculate_bleu_score_corpus(references, candidates):
     for i in candidates:
         i=word_tokenize(i)
 
-    # Calculate the corpus_level BLEU score, weights are already defaulted to 1/4 signifying BLEU-4
     #https://www.nltk.org/api/nltk.translate.bleu_score.html
     return corpus_bleu(references, candidates)*100
 
-'''
-This function calculates the BLEU-4 score for a single sentence. That means we would have a the caption and predicted caption provided as a list. The reference is a nested list, since this function
-is designed to handle multiple references that the candidate can match to and output the largest result
-'''
-def calculate_bleu_score_sentence(reference, candidate):    
+
+def calculate_bleu_score_sentence(reference:Union[str,List[str]], candidate:Union[str,List[str]])->float:
+    """This function calculates the BLEU-4 score for a single image caption prediction
     
-    # Example: [ref1a_sentence, ref1b_sentence]
-    #["The cat in the hat", "There is a cat on the mat"]
+    Args:
+        reference: A list containing the correct captions for an image i.e ['The cat is on the mat', 'there is a cat on the wall']
+        candidate: A list containing the predicted caption for an image i.e ['The cat is on the mat']
+    
+    Returns: The BLEU-4 score for the sentence
+    """    
+
+    #Tokenizes the reference and takes care of the two different input formats
     if isinstance(reference, list):
         reference=[word_tokenize(i) for i in reference]
     else:
         reference=[word_tokenize(reference)]
     
-    #Example: cand sentence
-    #"The cat sits on the mat"
+    #Tokenizes the candidate and takes care of the two different input formats
     if isinstance(candidate, list):
         candidate=word_tokenize(candidate[0])
     else:
         candidate=word_tokenize(candidate)
     
-    # Calculate the sentence_level BLEU score same as corpus_bleu but for a single sentence
     #https://www.nltk.org/api/nltk.translate.bleu_score.html
     return sentence_bleu(reference, candidate)*100
 
-'''
-The METEOR Score could also be passed as a list of references and a candidate for a single sentence, or a list of candidates if you have multiple. It will pick the largest score.
-https://hyperskill.org/learn/step/31112
-'''
-def calculate_meteor_score_sentence(reference, candidate):
-    #Example Reference: "The cat is on the mat"
-    #Example Candidate: The cat sits on the mat"
+
+def calculate_meteor_score_sentence(reference:Union[str,List[str]], candidate:Union[str,List[str]])->float:
+    """This function calculates the METEOR score for a single image caption prediction
+
+    Args:
+        reference: A list containing the correct captions for an image i.e ['The cat is on the mat', 'there is a cat on the wall']
+        candidate: A list containing the predicted caption for an image i.e ['The cat is on the mat']
+
+    Returns:
+        float: The METEOR score for the sentence
+    """
+    
+    #Tokenizes the reference and takes care of the two different input formats
     if isinstance(reference, list):
         reference=[word_tokenize(i) for i in reference]
     else:
         reference=[word_tokenize(reference)]
-        
+
+    #Tokenizes the candidate and takes care of the two different input formats
     if isinstance(candidate, list):
         candidate=word_tokenize(candidate[0])
     else:
         candidate=word_tokenize(candidate)
+    
+    #https://hyperskill.org/learn/step/31112
     return meteor_score(reference, candidate)*100
 
-'''
-While its unclear how to do corpus METEOR score, one common suggestion is just to average for all sentences
-'''
-def calculate_meteor_score_corpus(references,candidates):
-    #Example: [[ref1a sentence, ref1bsentence], [ref2a sentence, ref2b sentence]]
-    #['The cat is on the mat', 'there is a cat on the wall'],['The cat is not on the mat', 'there is a dog on the wall']]
 
-    #Example: [cand1 sentence, cand2 sentence]
-    #['The cat is on the mat','The cat is not on the mat']
+def calculate_meteor_score_corpus(references:List[List[str]], candidates:List[str])->float:
+    """This function calculates the METEOR score for a corpus of sentences, that is multiple candidates.
 
+    Args:
+        references: A list containing a list of correct captions for each image i.e [['The cat is on the mat', 'there is a cat on the wall'],['The cat is not on the mat', 'there is a dog on the wall']]
+        candidates: A list containing the predicted captions for each image i.e ['The cat is on the mat','The cat is not on the mat']
+
+    Returns:
+        float: The METEOR score for the entire corpus
+    """
+    #Asserts to ensure the passed parameters follow the correct format
     assert len(references) == len(candidates), "The lengths of references and candidates must be the same"
     assert isinstance(references,list), "References must be a list as it is looking at multiple captions"
     assert isinstance(candidates,list), "Candidates must be a list as it is looking at multiple captions"
 
-    meteor_scores = []
+    #Take the average of the METEOR scores for the batch of captions
+    meteor_total=0
+    n=len(references)
     for i in range(len(references)):
-        meteor_scores.append(calculate_meteor_score_sentence(references[i],candidates[i]))
-    return sum(meteor_scores)/len(meteor_scores)*100
+        meteor_total+=(calculate_meteor_score_sentence(references[i],candidates[i]))
+    return (meteor_total/n)
 
-'''
-Calculates the Rouge-L score for any number of sentences
-https://clementbm.github.io/theory/2021/12/23/rouge-bleu-scores.html#:~:text=In%20its%20simplest%20form%20ROUGE%20score%20is%20the,the%20denominator%20ROUGE%20is%20a%20recall%20oriented%20metric.
-'''
-def calculate_rouge_score(reference,candidate):
-    #Example: [ref1a_sentence, ref1b_sentence] or [[ref1a_sentence, ref1b_sentence], [ref2a_sentence, ref2b_sentence]]
-    #["The cat in the hat", "There is a cat on the mat"] or [["The cat in the hat", "There is a cat on the mat"], ["The cat is not on the mat", "There is a dog on the wall"]]
-    #Example: cand sentence or [cand1_sentence, cand2_sentence]
-    #"The cat sits on the mat" or ["The cat sits on the mat", "The cat is on the mat"]
 
+def calculate_rouge_score(references:List[List[str]], candidates:List[str])->float:
+    """This function calculates the ROUGE-L score for a corpus of sentences, that is multiple candidates. If you need one caption, just pass a list with one element.
+
+    Args:
+        references: A list containing a list of correct captions for each image i.e [['The cat is on the mat', 'there is a cat on the wall'],['The cat is not on the mat', 'there is a dog on the wall']]
+        candidates: A list containing the predicted captions for each image i.e ['The cat is on the mat','The cat is not on the mat']
+
+    Returns:
+        float: The ROUGE-L score for the entire corpus
+    """
+    #Loading in the route score
     rouge=evaluate.load('rouge')
-    assert len(reference) == len(candidate), "The lengths of references and candidates must be the same"
-    if not isinstance(reference[0],list):
-        reference=[[sentence]for sentence in reference]
-    if not isinstance(candidate,list):
-        candidate=[candidate]
+    #Ensure the same amount of references and candidates
+    assert len(references) == len(candidates), "The lengths of references and candidates must be the same"
 
-    return rouge.compute(predictions=candidate, references=reference)['rougeL']*100
+    #If the reference is not a list assume its one caption and make it a list
+    if not isinstance(references[0],list):
+        references=[[sentence]for sentence in references]
+
+    #If the candidate is not a list assume its one caption and make it a list
+    if not isinstance(candidates,list):
+        candidates=[candidates]
+
+    #https://clementbm.github.io/theory/2021/12/23/rouge-bleu-scores.html#:~:text=In%20its%20simplest%20form%20ROUGE%20score%20is%20the,the%20denominator%20ROUGE%20is%20a%20recall%20oriented%20metric.
+    return rouge.compute(predictions=candidates, references=references)['rougeL']*100
+
 
 '''
-Using AAC-metrics package to output cider-d score
-'''
-def calculate_cider_d_score(references,candidates):
+def calculate_cider_d_score(references:List[List[str]], candidates:List[str])->object:
+    """This function calculates the CIDEr-D score for a corpus of sentences, that is multiple candidates. If you need one caption, just pass a list with one element.
+
+    Args:
+        references: A list containing a list of correct captions for each image i.e [['The cat is on the mat', 'there is a cat on the wall'],['The cat is not on the mat', 'there is a dog on the wall']]
+        candidates: A list containing the predicted captions for each image i.e ['The cat is on the mat','The cat is not on the mat']
+
+    Returns:
+        float: The CIDEr-D score for the entire corpus
+    """
+    #Need to install aac-metrics packages one time
     try:
         # Using shell=True to run the command through the shell
         result = subprocess.run("aac-metrics-download", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         print("Output:", result.stdout)
     except subprocess.CalledProcessError as e:
         print("Error occurred:", e.stderr)
+
+    #If the reference is not a list of lists, assume its for one caption and make it a list of lists
     if not isinstance(references[0],list):
         references=[[sentence]for sentence in references]
+
+    #CIDEr-D score only works for more than 2 captions
     assert not isinstance(candidates,str), "Must be more than 2 candidates to calculate cider-d"
     assert len(candidates)>1, "Must be more than 2 candidates to calculate cider-d"
+
+    #Use the aac-metrics package to calculate the CIDEr-D score
     candidates = preprocess_mono_sents(candidates)
     mult_references = preprocess_mult_sents(references)
     corpus_scores, sents_scores = cider_d(candidates, mult_references)
     return corpus_scores
-
-def test_metrics():
-    candidate= ["a man is speaking", "rain falls"]
-    reference = [["a man speaks.", "someone speaks.", "a man is speaking while a bird is chirping in the background"], ["rain is falling hard on a surface"]]
-    #reference = ['this is small test']
-    #candidate = ['this is a test']
-    print(calculate_cider_d_score(reference, candidate))
-
-
-#test_metrics()
+'''
 
